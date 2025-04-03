d830082cc64ce08a5ad26b3631118b75e5b2327e
CaoMeiYouRen <996881204@qq.com>
2025-04-03 20:31:56 +0800

    feat: 添加 Cloudflare 依赖并实现 CDN 刷新功能

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { S3Client, PutObjectCommand, PutObjectCommandInput } from '@aws-sdk/clie
 import fs from 'fs-extra'
 import mime from 'mime-types'
 import dotenv from 'dotenv'
+import Cloudflare from 'cloudflare'
 
 dotenv.config({
     path: [
@@ -17,6 +18,7 @@ const ACCOUNT_ID = process.env.ACCOUNT_ID || ''
 const ACCESS_KEY_ID = process.env.ACCESS_KEY_ID || ''
 const SECRET_ACCESS_KEY = process.env.SECRET_ACCESS_KEY || ''
 const BUCKET_NAME = process.env.BUCKET_NAME || ''
+const BASE_URL = process.env.BASE_URL || ''
 
 const S3 = new S3Client({
     region: 'auto',
@@ -26,6 +28,20 @@ const S3 = new S3Client({
         secretAccessKey: SECRET_ACCESS_KEY,
     },
 })
+
+async function cloudflarePurgeCaches(files: string[]) {
+    try {
+        const client = new Cloudflare({
+            apiEmail: process.env['CLOUDFLARE_EMAIL'], // This is the default and can be omitted
+            apiKey: process.env['CLOUDFLARE_API_KEY'], // This is the default and can be omitted
+        })
+        const CLOUDFLARE_ZONE_ID = process.env['CLOUDFLARE_ZONE_ID']
+        const response = await client.cache.purge({ zone_id: CLOUDFLARE_ZONE_ID, files })
+        return response
+    } catch (error) {
+        logger.error(error)
+    }
+}
 
 async function upload(filepath: string, rootpath: string, prefix: string) {
     try {
@@ -56,6 +72,11 @@ async function start() {
     for (const file of files) {
         if (await fs.pathExists(file)) {
             await upload(file, 'sponsorkit', 'sponsorkit')
+            const url = new URL(file, BASE_URL).toString()
+            logger.info(`文件：${file} 上传成功！url：${url}`)
+            // CDN 刷新
+            const response = await cloudflarePurgeCaches([url])
+            logger.info(`文件：${file} CDN 刷新成功！code：${response}`)
         }
     }
     logger.info('全部文件上传完成！')
